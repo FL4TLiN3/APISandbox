663e6f01f4e1a7e4db6f2be5596361e78af83f51
HiranoMasaaki <lambda.groove@gmail.com>
2015-12-14 17:44:07 +0900

    Initial Commit

---
+++ Sandbox/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Sandbox
+//
+//  Created by Masaaki Hirano on 2015/12/14.
+//  Copyright © 2015年 codelust. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
